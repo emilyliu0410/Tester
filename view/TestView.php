24da5ba71b9a2fcf2b0fe763ec0223e1d6cdebe1
emily_liu <yingpik67@hotmail.com>
2018-06-08 12:32:48 +0800

    css js

--- view/TestView.php
+++ view/TestView.php
@@ -2,10 +2,11 @@
 <html>
 <head>
 	<title>Emily Liu</title>
-
+<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-	<p>1234567890<p>
+
+	<p>123456789011<p>
 	<?php 
 		if($result) {
 			while($row = $result->fetch_array()){
@@ -18,4 +19,5 @@
 	?>
 
 </body>
+<script src="https://cdn.bootcss.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </html>
